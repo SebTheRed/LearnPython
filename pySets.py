50d99b9e2a40ca713ea64ee6747705094f801e05
SebThRed <91200399+SebTheRed@users.noreply.github.com>
2023-04-22 20:29:17 -0400

    sets 28

---
+++ pySets.py
@@ -0,0 +1,23 @@
+
+
+# Challenge 28
+'''
+Write a Python program that takes two sets and performs the following operations:
+
+Find the union of the two sets
+Find the intersection of the two sets
+Find the difference of the two sets (set1 - set2)
+Determine if set1 is a subset of set2
+Determine if set2 is a superset of set1
+
+mySet.add(value) #Adds a value to the set.
+mySet.remove(value) #Removes a value from the set.
+
+mySet.union(): #returns a new set containing all elements from both sets
+mySet.intersection(): #returns a new set containing only elements that are in both sets
+mySet.difference(): #returns a new set containing only elements that are in the first set but not in the second set
+mySet.issubset(): #returns True if all elements of a set are contained in another set
+mySet.issuperset(): #returns True if a set contains all elements of another set
+
+'''
+#Unlike those lists & tuples, sets cannot have multiple occurrences of the same element. 
