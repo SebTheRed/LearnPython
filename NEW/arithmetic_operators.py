963a0189d9067d5fc4fef31522fcb0af961c02c1
SebTheRed <seb.belfonti@gmail.com>
2023-12-01 17:31:58 -0500

    4 more lessons!

---
+++ NEW/arithmetic_operators.py
@@ -0,0 +1,18 @@
+# Addition (+): Adds two operands.
+# Subtraction (-): Subtracts the right operand from the left operand.
+# Multiplication (*): Multiplies two operands.
+# Division (/): Divides the left operand by the right operand and returns a floating-point number.
+
+# Write a Python script to calculate the area of a rectangle. Declare two variables length and width with values of your choice.
+# Calculate the area using the multiplication operator and store the result in a variable named area.
+# Print the result in a format like "The area of the rectangle is [area]."
+# As an extra step, calculate the perimeter of the rectangle (Perimeter = 2*(length + width)) and print it out.
+
+length = 10
+width = 42
+
+sq_area = length * width
+perimeter = 2*(length + width)
+
+print("The area of the rectangle is: ", sq_area)
+print("The perimeter of the rectangle is: ", perimeter)
