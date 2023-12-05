91082123b71dd0a2da7a6a546e62b3701c09cc84
SebTheRed <seb.belfonti@gmail.com>
2023-12-05 15:36:43 -0500

    finished tuples

---
+++ NEW/tuples.py
@@ -0,0 +1,10 @@
+# CHALLENGE:
+# For practice, create a tuple representing a point in 3D space, for example, (x, y, z). Then:
+# Print each coordinate using indexing.
+# Try to change one of the coordinates to see what happens (you should encounter a TypeError).
+
+tupleVariable = (12, 97, -2)
+print("X: ", tupleVariable[0])
+print("Y: ", tupleVariable[1])
+print("Z: ", tupleVariable[2])
+# tupleVariable[1] = 20
