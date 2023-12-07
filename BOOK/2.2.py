a91537759bffe58671aea193672651693361e446
SebTheRed <seb.belfonti@gmail.com>
2023-12-07 17:00:37 -0500

    did first challenge of 2.2

---
+++ BOOK/2.2.py
@@ -0,0 +1,19 @@
+# Notes:
+# Python has 6 types of operators:
+# Arithmetic, Comparison, Logical, Assignment, Identity, Membership
+# Arithmetic is your math operators.
+# Comparison is discerning the difference between values.
+# Logical is simply three methods (and, or, not) that checks between boolean values.
+# Assignment gives a variable its value, additional arithmetic can be included.
+# Identity is used to determine if an object is the same as itself. simply (is, is not) operators.
+
+#1. Exploring Arithmetic Operators: Create two variables and use each arithmetic operator. Experiment with different values (including negative numbers and zero).
+a = 67
+b = -27
+print("Subtraction: ", type(a-b), a-b)
+print("Addition: ", type(b+a), b+a)
+print("Multiplication: ", type(a*b), a*b)
+print("Division: ", type(a/b), a/b)
+
+#2. Logical Puzzles: Given three boolean variables x, y, and z, write expressions using logical operators that return True only if exactly two of the three variables are True.
+#   Bonus: Use comparison operators to compare the results of different arithmetic expressions (e.g., check if the sum of two numbers is greater than their product).
