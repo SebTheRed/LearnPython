952730b951d12e0e55f2dab73bbfe811362886c9
SebTheRed <seb.belfonti@gmail.com>
2024-09-08 09:33:08 -0400

    y

---
+++ challenges/2.5.py
@@ -0,0 +1,4 @@
+# Challenge:
+# Write a program that converts temperature from Fahrenheit to Celsius. Use string formatting to output a message like "68°F is 20°C."
+
+# Create a program that takes a list of items with their prices and prints a receipt with a total amount. Use string formatting to align prices neatly.
